a81b1d5862633b7dd9fbffa6299eccbf3b316974
Sebastian Olenski <solenski@outlook.com>
2020-11-01 17:47:50 +0100

    store init for categories

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, APP_INITIALIZER } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
-import { reducers, metaReducers } from './reducers';
-
+import { reducers } from './reducers';
+import { EffectsModule } from '@ngrx/effects';
+import { AppConfigService } from './app-config.service'
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot({}, {}),
-    StoreModule.forRoot(reducers, {
-      metaReducers
-    })
+    StoreModule.forRoot(reducers),
+    EffectsModule.forRoot([]),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/modules/categories/categories.module.ts
+++ src/app/modules/categories/categories.module.ts
@@ -4,13 +4,16 @@ import { CommonModule } from '@angular/common';
 import { CategoriesRoutingModule } from './categories-routing.module';
 import { CategoriesSmartComponent } from './categories-smart/categories-smart.component';
 import { CategoriesDumbComponent } from './categories-dumb/categories-dumb.component';
-
-
+import { StoreModule } from '@ngrx/store';
+import { reducers } from './state/reducers';
 @NgModule({
   declarations: [CategoriesSmartComponent, CategoriesDumbComponent],
   imports: [
     CommonModule,
-    CategoriesRoutingModule
-  ]
+    StoreModule.forFeature(CategoriesModule.FeatureName, reducers),
+    CategoriesRoutingModule,
+  ],
 })
-export class CategoriesModule { }
+export class CategoriesModule {
+  static readonly FeatureName = 'Categories';
+}
